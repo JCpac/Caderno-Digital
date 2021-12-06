a0114ff73e49804031bec71bdf5b2ca3179aef3b
JCpac <joaoc.costa@outlook.pt>
2021-12-06 00:20:28 +0000

    Styled section headers
    Section headers are now top-sticky
    Changed side-padding for "p", "ul" and "ol" elements, to prevent them from being seen under the section headers' round corners
    Added margin at the bottom of the last element of each section, to make the sticky headers cover possible image shadows
    Changed "img-single" CSS class to "image-single" for consistency
    Hiding a section scrolls the page to place its header at the top of the screen

--- app.js
+++ app.js
@@ -6,9 +6,18 @@ let config = {
     "h2-initialy-closed": true
 }
 
+function sectionHeaderClickHandler(event) {
+    h2 = event.target;
+
+    h2.classList.toggle(cssClasses["h2-toggle"]);
+
+    if (h2.classList.contains(cssClasses["h2-toggle"]))
+        h2.scrollIntoView(true);
+}
+
 function setupSectionHeaders(h2List) {
     h2List.forEach(h2 => {
-        h2.addEventListener("click", event => { h2.classList.toggle(cssClasses["h2-toggle"]); });
+        h2.addEventListener("click", event => sectionHeaderClickHandler(event));
 
         let h2Space = document.createElement("span");
         h2Space.innerHTML = "&emsp;";
